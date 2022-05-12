47259872c4f04f38e45584849ce9122781b72324
Sacha BOUDHOU <72568998+SachaBoudhou@users.noreply.github.com>
2022-05-13 00:22:40 +0200

    add Canni, Dilation and Erosion

--- OpenCV-Cours/TestFirst.cpp
+++ OpenCV-Cours/TestFirst.cpp
@@ -259,47 +259,95 @@ using namespace std;
 //    imshow("Dilation Demo", dilation_dst);
 //}
 
-//////// Erosion /////////
 
-Mat src, erosion_dst;
-int erosion_elem = 0;
-int erosion_size = 0;
-int const max_elem = 2;
-int const max_kernel_size = 21;
-const string path = "D:/OpenCV-Cours/test.jpg";
 
-void Erosion(int, void*);
+////////////////// Erosion //////////////
 
-int main(int argc, char** argv)
-{
-    src = imread(path, IMREAD_COLOR);
-    if (src.empty())
-    {
-        cout << "Could not open or find the image!\n" << endl;
-        cout << "Usage: " << argv[0] << " <Input image>" << endl;
-        return -1;
-    }
-    namedWindow("Erosion Demo", WINDOW_AUTOSIZE);
-    createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Erosion Demo",
-        &erosion_elem, max_elem,
-        Erosion);
-    createTrackbar("Kernel size:\n 2n +1", "Erosion Demo",
-        &erosion_size, max_kernel_size,
-        Erosion);
-    Erosion(0, 0);
-    waitKey(0);
-    return 0;
-}
+//Mat src, erosion_dst;
+//int erosion_elem = 0;
+//int erosion_size = 0;
+//int const max_elem = 2;
+//int const max_kernel_size = 21;
+//const string path = "D:/OpenCV-Cours/test.jpg";
+//
+//void Erosion(int, void*);
+//
+//int main(int argc, char** argv)
+//{
+//    src = imread(path, IMREAD_COLOR);
+//    if (src.empty())
+//    {
+//        cout << "Could not open or find the image!\n" << endl;
+//        cout << "Usage: " << argv[0] << " <Input image>" << endl;
+//        return -1;
+//    }
+//    namedWindow("Erosion Demo", WINDOW_AUTOSIZE);
+//    createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Erosion Demo",
+//        &erosion_elem, max_elem,
+//        Erosion);
+//    createTrackbar("Kernel size:\n 2n +1", "Erosion Demo",
+//        &erosion_size, max_kernel_size,
+//        Erosion);
+//    Erosion(0, 0);
+//    waitKey(0);
+//    return 0;
+//}
+//
+//void Erosion(int, void*)
+//{
+//    int erosion_type = 0;
+//    if (erosion_elem == 0) { erosion_type = MORPH_RECT; }
+//    else if (erosion_elem == 1) { erosion_type = MORPH_CROSS; }
+//    else if (erosion_elem == 2) { erosion_type = MORPH_ELLIPSE; }
+//    Mat element = getStructuringElement(erosion_type,
+//        Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+//        Point(erosion_size, erosion_size));
+//    erode(src, erosion_dst, element);
+//    imshow("Erosion Demo", erosion_dst);
+//}
 
-void Erosion(int, void*)
-{
-    int erosion_type = 0;
-    if (erosion_elem == 0) { erosion_type = MORPH_RECT; }
-    else if (erosion_elem == 1) { erosion_type = MORPH_CROSS; }
-    else if (erosion_elem == 2) { erosion_type = MORPH_ELLIPSE; }
-    Mat element = getStructuringElement(erosion_type,
-        Size(2 * erosion_size + 1, 2 * erosion_size + 1),
-        Point(erosion_size, erosion_size));
-    erode(src, erosion_dst, element);
-    imshow("Erosion Demo", erosion_dst);
-}
+
+////////// Other morphology operation ////////////////////////
+
+//Mat src, dst;
+//int morph_elem = 0;
+//int morph_size = 0;
+//int morph_operator = 0;
+//int const max_operator = 4;
+//int const max_elem = 2;
+//int const max_kernel_size = 21;
+//const char* window_name = "Morphology Transformations Demo";
+//const string path = "D:/OpenCV-Cours/test.jpg";
+//void Morphology_Operations(int, void*);
+//
+//int main(int argc, char** argv)
+//{
+//    src = imread(path, IMREAD_COLOR);
+//
+//    if (src.empty())
+//    {
+//        std::cout << "Could not open or find the image!\n" << std::endl;
+//        std::cout << "Usage: " << argv[0] << " <Input image>" << std::endl;
+//        return EXIT_FAILURE;
+//    }
+//
+//    namedWindow(window_name, WINDOW_AUTOSIZE); // Create window
+//    createTrackbar("Operator:\n 0: Opening - 1: Closing  \n 2: Gradient - 3: Top Hat \n 4: Black Hat", window_name, &morph_operator, max_operator, Morphology_Operations);
+//    createTrackbar("Element:\n 0: Rect - 1: Cross - 2: Ellipse", window_name,
+//        &morph_elem, max_elem,
+//        Morphology_Operations);
+//    createTrackbar("Kernel size:\n 2n +1", window_name,
+//        &morph_size, max_kernel_size,
+//        Morphology_Operations);
+//    Morphology_Operations(0, 0);
+//    waitKey(0);
+//    return 0;
+//}
+//void Morphology_Operations(int, void*)
+//{
+//    // Since MORPH_X : 2,3,4,5 and 6
+//    int operation = morph_operator + 2;
+//    Mat element = getStructuringElement(morph_elem, Size(2 * morph_size + 1, 2 * morph_size + 1), Point(morph_size, morph_size));
+//    morphologyEx(src, dst, operation, element);
+//    imshow(window_name, dst);
+//}
